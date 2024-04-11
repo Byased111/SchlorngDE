7b3c9f6dbad4f8086c3fde61e2cf4b8d614bf194
Byased111 <123678861+Byased111@users.noreply.github.com>
2024-04-11 15:19:07 -0400

    Add files via upload

---
+++ Pipe/lua/Config.lua
@@ -0,0 +1,27 @@
+local Config = {
+	Graphics = {
+		HeaderBackgroundColor = 60,
+		HeaderForegroundColor = 15,
+
+		HighlightBackgroundColor = 240,
+		HighlightForegroundColor = 11,
+
+		LinesUseBgColor = false,
+		LineSeparator = " ",
+		LineBackgroundColor = 0,
+		LineForegroundColor = 104,
+
+		ScreenRows = 30
+	},
+
+	--[[
+	Binds = { --must use ansi esc codes
+		
+	},
+	AutoExec = {
+		
+	}]] --ignore these, I should probably get rid of these ngl
+	
+}
+
+return Config
---
+++ Pipe/lua/Enums.lua
@@ -0,0 +1,18 @@
+--Enums just to make the process of writing this program nicer
+local Enums = {
+	Modes = {
+		Mark = 1,
+		Text = 2,
+		Motion = 3
+	},
+	ProgramModes = {
+		Sandbox = 1,
+		FileOpen = 2
+	},
+	Extra = {
+		VeryBig = 4294967295 --unsigned long --I forgot to use this
+
+	}
+}
+
+return Enums
